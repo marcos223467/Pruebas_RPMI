87ca23824ed83bc2c47896d685a1274bd98de8d2
marcos223467 <marcos223467@gmail.com>
2024-05-09 12:44:23 +0200

    Cambios en el repo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float vel, velMax, f_salto, f_dash, dash_CD, cool_down_dash;
     private bool canJump, salto, dash;
-    [SerializeField] private LayerMask suelo;
+    [SerializeField] private LayerMask suelo, pared;
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
+    private Animator _anim;
 
     private Vector2 inputDirection;
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
         canJump = true;
         salto = false;
         inputDirection = new Vector2(0, 0);
@@ -58,15 +60,21 @@ public class PlayerController : MonoBehaviour
         if (_rb.velocity.magnitude <= velMax)
             _rb.AddForce(inputDirection * vel, ForceMode2D.Force);
         
+        if(_rb.velocity.magnitude <= 0.3f)
+            _anim.SetBool("Run", false);
+        else
+            _anim.SetBool("Run", true);
     }
 
     private void Jump()
     {
         if (salto)
         {
+            _anim.SetTrigger("Jump");
             _rb.AddForce(new Vector2(0,1) * f_salto, ForceMode2D.Impulse);
             canJump = false;
             salto = false;
+            _anim.SetBool("Caer", true);
         }
     }
 
@@ -82,9 +90,18 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.layer == Mathf.Log(suelo.value, 2)) // Chocamos con el suelo
+        if (other.gameObject.layer == Mathf.Log(suelo.value, 2f)) // Chocamos con el suelo
         {
             canJump = true;
+            _anim.SetBool("Caer", false);
+            _anim.SetBool("Wall", false);
+        }
+        
+        if (other.gameObject.layer == Mathf.Log(pared.value, 2)) // Chocamos con el suelo
+        {
+            canJump = true;
+            _anim.SetBool("Caer", false);
+            _anim.SetBool("Wall", true);
         }
     }
 }
